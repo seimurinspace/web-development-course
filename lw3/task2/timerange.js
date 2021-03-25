e908aa0af71a7993d2d50b924e1f17aa23132ded
Макс <seimurinspace@mail.ru>
2021-03-26 00:47:50 +0300

    fixed problems with validation by belousov m.s.

--- lw3/task2/timerange.js
+++ lw3/task2/timerange.js
@@ -1,5 +1,9 @@
 function isTimeRangesIntersect(timeRange1, timeRange2) {
-  if (timeRange1 !== null && timeRange2 !== null) {
+  if (/^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$/.test(timeRange1[0]) &&
+    /^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$/.test(timeRange1[1]) &&
+    /^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$/.test(timeRange2[0]) &&
+    /^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$/.test(timeRange2[1])) {
+
     var result = timeRange1[1] >= timeRange2[0];
     return result;
   } else {
