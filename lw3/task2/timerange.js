8d138a9738e65a0b62fda0cfb9a6a629249e699c
Макс <seimurinspace@mail.ru>
2021-03-29 23:37:48 +0300

    fixed problems by belousov m.s.

--- lw3/task2/timerange.js
+++ lw3/task2/timerange.js
@@ -1,8 +1,11 @@
 function isTimeRangesIntersect(timeRange1, timeRange2) {
-  if (/^([0-1]?[0-9]|2[0-3]):([0-5][0-9])(:[0-5][0-9])?$/.test(timeRange1[0]) &&
+  if (Array.isArray(timeRange1) && Array.isArray(timeRange2) &&
+    /^([0-1]?[0-9]|2[0-3]):([0-5][0-9])(:[0-5][0-9])?$/.test(timeRange1[0]) &&
     /^([0-1]?[0-9]|2[0-3]):([0-5][0-9])(:[0-5][0-9])?$/.test(timeRange1[1]) &&
     /^([0-1]?[0-9]|2[0-3]):([0-5][0-9])(:[0-5][0-9])?$/.test(timeRange2[0]) &&
-    /^([0-1]?[0-9]|2[0-3]):([0-5][0-9])(:[0-5][0-9])?$/.test(timeRange2[1])) {
+    /^([0-1]?[0-9]|2[0-3]):([0-5][0-9])(:[0-5][0-9])?$/.test(timeRange2[1])
+
+  ) {
 
     var result = timeRange1[1] >= timeRange2[0];
     return result;
