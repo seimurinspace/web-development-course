afdf37b8c6acce2755503049a33a7ccc47b81796
Макс <seimurinspace@mail.ru>
2021-05-13 12:38:55 +0300

    create index.html and script.js with their structure (fixed validation and added new method RegExp) by Belousov M.S

---
+++ lw4/task4/script.js
@@ -0,0 +1,9 @@
+String.prototype.countCoincidence = function (str) {
+  var regExp = new RegExp(str, 'gi'); //применил метод RegExp с флажками "gi" (i = независимость от регистра), (g = поиск всех совпадений)
+  let searchChar = this.match(regExp);
+  if (searchChar === null) { //введена валидация, если функция не найдёт совпадений, вернёт 0, без неё - TypeError
+    return 0;
+  } else {
+    return searchChar.length;
+  }
+}
