b0f945e4815a7766beef8e138d0461fef8593b0e
Макс <seimurinspace@mail.ru>
2021-05-20 18:53:12 +0300

    fixed problems by Belousov M.S.

--- lw4/task4/script.js
+++ lw4/task4/script.js
@@ -1,7 +1,7 @@
 String.prototype.countCoincidence = function (str) {
   var regExp = new RegExp(str, 'gi'); //применил метод RegExp с флажками "gi" (i = независимость от регистра), (g = поиск всех совпадений)
   let searchChar = this.match(regExp);
-  if (searchChar === null) { //введена валидация, если функция не найдёт совпадений, вернёт 0, без неё - TypeError
+  if (searchChar === null) { //введена валидация, если функция не найдёт совпадений, вернёт 0, без валидации - TypeError
     return 0;
   } else {
     return searchChar.length;
